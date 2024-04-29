fb969f6e7247f6d169a10de7131414daf29d90a2
Jude Amboise <judeamboise@Judes-MacBook-Air.local>
2024-04-29 11:07:54 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,6 @@
+
+
+
+
+
+////warning: unable to access '/Users/judeamboise/.config/git/attributes': Permission denied, how to fix this? // solution: sudo chown -R $(whoami) ~/.config // https://stackoverflow.com/questions/53899687/warning-unable-to-access-users-username-config-git-attributes-permission-denied
